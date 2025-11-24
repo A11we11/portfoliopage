c11fe33f7d75bac5fba39e0341b488d997ee1215
0llwell <ogbonnaallwell1999@gmail.com>
2025-11-24 20:47:21 +0100

    added mapbox project

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,24 +3,32 @@ export const projects = [
     title: "E-commerce Platform",
     description:
       "Modern e-commerce solution with React, Node.js, and Paypal integration. Features include user authentication, cart management, and admin dashboard.",
-    tech: ["React", "Node.js", "MongoDB", "Paypal", "TailwindCss", "Shadcn"],
+    tech: [
+      "React",
+      "Node.js",
+      "MongoDB",
+      "Paypal",
+      "TailwindCss",
+      "Shadcn",
+      "Typescript",
+    ],
     github: "https://github.com/A11we11/ecommerce",
     demo: "https://ecommerce-4d6f.vercel.app",
   },
   {
     title: "Food Delivery Platform",
     description:
-      "Modern e-commerce solution with React, Node.js, and Paypal integration. Features include user authentication, cart management, and admin dashboard.",
-    tech: ["React", "Express.js", "MongoDB", "Paystack", "Css"],
+      "A modern food delivery app that lets users browse restaurants, explore menus, place orders in real-time. Built with a clean UI and a smooth user experience.",
+    tech: ["React", "Express.js", "MongoDB", "Paystack", "Css", "Typescript"],
     github: "https://github.com/A11we11/Food-ordering-app",
     demo: "https://food-ordering-app-uc34.vercel.app",
   },
-  /*  {
-    title: "E-commerce Platform",
+  {
+    title: "city explorer map",
     description:
-      "Modern e-commerce solution with React, Node.js, and Paypal integration. Features include user authentication, cart management, and admin dashboard.",
-    tech: ["React", "Node.js", "MongoDB", "Stripe"],
-    github: "",
-    demo: "https://ecommerce-4d6f.vercel.app",
-  }, */
+      "This is a fully interactive map application powered by Mapbox GL JS and Next.js.",
+    tech: ["Next.js", "Tailwindcss", "Mapbox", "Typescript"],
+    github: "https://github.com/A11we11/mapbox_setup",
+    demo: "https://mapbox-setup.vercel.app",
+  },
 ];
